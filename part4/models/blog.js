fa8c1a658652ea3bf6366e055a7bc4cd6ec296d6
giuliano monti <giulianm@gmail.com>
2021-12-08 20:02:04 -0300

    vamos x partes sino we dont undersdtand very well

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,26 +1,13 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const blogSchema = new mongoose.Schema({
   title: String,
-  author: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   url: String,
   likes: Number,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 blogSchema.set("toJSON", {
